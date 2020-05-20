392a2b1319fc3f3f43a8a99b9d5f165edfc71c33
root <knjackson@upei.ca>
2020-05-20 09:43:33 -0400

    5.4 update

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 $id = 'evntlog';
 $version = '0.0.01';
 $ilias_min_version = '5.0.0';
-$ilias_max_version = '5.3.999';
+$ilias_max_version = '5.4.999';
 $responsible = 'JKN Inc.';
 $responsible_mail = 'itstaff@jkninc.com';
 ?>
